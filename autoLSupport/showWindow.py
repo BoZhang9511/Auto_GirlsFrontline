ab9cfc4c066e0b2b46a66ae0498ce6dd57a83c12
LukeZhang0 <bzhang95@163.com>
2021-01-29 10:41:55 +0800

    Delete autoLSupport directory

--- autoLSupport/showWindow.py
+++
@@ -1,5 +0,0 @@
-from autoLSupport import *
-
-showWindow()
-
-
--- autoLSupport/test.py
+++
@@ -1,8 +0,0 @@
-from autoLSupport import *
-
-
-time.sleep(0.5)
-img = getImage(MAIN_MENU_IMAGE_BOX)
-img.show()
-img.save("initial_IMG/main_menu.png")
-
