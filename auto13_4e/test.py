7a7bacf911508b7e78c953c3c05173bd7ae97304
LukeZhang0 <bzhang95@163.com>
2021-08-13 07:09:58 +0800

    1

--- auto13_4e/test.py
+++ auto13_4e/test.py
@@ -1,9 +1,9 @@
 from auto13_4e import *
 
 time.sleep(0.5)
-img = getImage(NAVIGATE_FACTORY_CLICK_BOX)
+img = getImage(RETURN_COMBAT_IMAGE_BOX)
 img.show()
 time.sleep(1)
 
 img = cv2.cvtColor(np.asarray(img),cv2.COLOR_RGB2BGR)
-#cv2.imwrite("initial_IMG/main_menu.png", img)
+#cv2.imwrite("initial_IMG/return_combat.png", img)
